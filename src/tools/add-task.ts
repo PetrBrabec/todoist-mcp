568b4a8a652884bf1c2f05b8d1a3b643df01e669
Petr Brabec <dev@petrbrabec.cz>
2025-04-20 22:48:01 +0200

    allow null

--- src/tools/add-task.ts
+++ src/tools/add-task.ts
@@ -3,6 +3,7 @@ import type { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { z } from 'zod'
 
 const nonEmptyString = z.string().transform((str) => (str.trim() === '' ? undefined : str))
+const transformToUndefined = <T>(v: T) => (v ? v : undefined)
 
 export function registerAddTask(server: McpServer, api: TodoistApi) {
     server.tool(
@@ -10,24 +11,40 @@ export function registerAddTask(server: McpServer, api: TodoistApi) {
         'Add a task to Todoist',
         {
             content: z.string(),
-            description: nonEmptyString.optional(),
-            projectId: nonEmptyString.optional().describe('The ID of a project to add the task to'),
+            description: nonEmptyString.optional().nullable().transform(transformToUndefined),
+            projectId: nonEmptyString
+                .optional()
+                .nullable()
+                .transform(transformToUndefined)
+                .describe('The ID of a project to add the task to'),
             assigneeId: nonEmptyString
                 .optional()
+                .nullable()
+                .transform(transformToUndefined)
                 .describe('The ID of a project collaborator to assign the task to'),
             priority: z
                 .number()
                 .min(1)
                 .max(4)
                 .optional()
+                .nullable()
+                .transform(transformToUndefined)
                 .describe('Task priority from 1 (normal) to 4 (urgent)'),
-            labels: z.array(z.string()).optional(),
-            parentId: nonEmptyString.optional().describe('The ID of a parent task'),
+            labels: z.array(z.string()).optional().nullable().transform(transformToUndefined),
+            parentId: nonEmptyString
+                .optional()
+                .nullable()
+                .transform(transformToUndefined)
+                .describe('The ID of a parent task'),
             deadlineDate: nonEmptyString
                 .optional()
+                .nullable()
+                .transform(transformToUndefined)
                 .describe("Specific date in YYYY-MM-DD format relative to user's timezone."),
             deadlineLang: nonEmptyString
                 .optional()
+                .nullable()
+                .transform(transformToUndefined)
                 .describe('2-letter code specifying language of deadline.'),
         },
         async ({
